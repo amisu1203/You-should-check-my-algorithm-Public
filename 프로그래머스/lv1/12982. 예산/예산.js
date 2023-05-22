65c8bf331dd7c69d7052681ec67cb57c78bd774c
jisu <doh_0112@naver.com>
2023-05-22 10:22:45 +0900

    [level 1] Title: 예산, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -0,0 +1,12 @@
+function solution(d, budget) {
+    var answer = 0;
+    d = d.sort((a,b)=>a-b);
+    for (let i = 0; i < d.length; i++) {
+        if (budget >= d[i]) {
+            budget = budget - d[i];
+            answer++;
+        }
+        
+    }
+    return answer;
+}
